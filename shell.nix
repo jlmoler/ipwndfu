802451a74b2ede8e49bd40243824f4237e8c6f8a
ilian <ilian@tuta.io>
2021-08-27 18:04:02 +0200

    Add arm64 cross-compiler to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
-with (import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz) {});
+let
+  nixpkgs = (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz);
+  arm64cross = import nixpkgs {
+    crossSystem = (import "${nixpkgs}/lib").systems.examples.aarch64-multiplatform;
+  };
+in
+
+with (import nixpkgs {});
 
 pkgs.mkShell {
-  buildInputs = [
+  nativeBuildInputs = [
     (python2.withPackages(ps: with ps; [ pyusb ]))
     gcc-arm-embedded
+    arm64cross.stdenv.cc
   ];
 }
